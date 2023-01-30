63e65a0d83e8b7d49d6bcbaafea0917b4f107042
РОМАН <themorozushka@gmail.com>
2023-01-30 20:05:12 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    private static void task1() {
+
+    }
+
+    private static void task2() {
+    }
+
+    private static void task3() {
+    }
+
+}
